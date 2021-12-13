20f7754d2dec2f26cb4917f538227dd26d0a259d
ingramwd <Ingramwd01@gmail.com>
2021-12-12 22:05:04 -0500

    added 1 day to 5 day forecast fixed bugs and display issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,35 @@ var temp = document.querySelector('.temp');
 var humid = document.querySelector('.humidity');
 var wind = document.querySelector('.wind');
 
-// var oneDay = document.querySelector('.1-days');
-// var twoDay = document.querySelector('.2-days');
-// var threeDay = document.querySelector('.3-days');
-// var fourDay = document.querySelector('.4-days');
-// var fiveDay = document.querySelector('.5-days');
 
 var displayCurrentWeather = function (data) {
-    var cityName = data['name']
-    var temperature = data['main']['temp'];
-    var humidity = data['main']['humidity'];
-    var windSpeed = data['wind']['speed'];
+    var cityName = data.city.name + " " + moment().format('L')
+    var temperature = data.list[0]['main']['temp'];
+    var humidity = data.list[0]['main']['humidity'];
+    var windSpeed = data.list[0]['wind']['speed'];
 
-    nameEl.innerHtml = cityName;
-    temp.innerHTML = temperature + "°F";
-    humid.innerHTML = humidity + "%";
-    wind.innerHTML = windSpeed;
+    console.log(cityName);
+
+    nameEl.textContent = cityName;
+    temp.innerHTML = "Temperature: " + temperature + "°F";
+    humid.innerHTML = "Humidity: " + humidity + "%";
+    wind.innerHTML = "Wind: " + windSpeed + "mph";
+
+    console.log(nameEl);
 }
 
 var searching = function (event) {
     event.preventDefault();
     console.log(event)
     getWeatherData(userInputEl.value);
+    fiveDay(userInputEl.value);
 
 };
 
 var getWeatherData = function (input) {
 
     // formate openweathermap api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=Imperial&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + apiKey + "&units=imperial";
     console.log(apiUrl);
 
 
@@ -58,26 +58,45 @@ var getWeatherData = function (input) {
 citySearch.addEventListener("submit", searching);
 
 
-// var fiveDay = function (input) {
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=f95df33b8a4b905edf0dbd58e095f0f3";
+var fiveDay = function (input) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + apiKey + "&units=imperial";
 
-//     fetch(apiUrl)
-//         .then(function (response) {
-//             if (response.ok) {
-//                 response.json().then(function (data) {
-//                     displayFive(data);
-//                 });
-//             }
-//             else {
-//                 alert("Error something went Wrong")
-//             }
-//         });
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayFive(data);
+                });
+            }
+            else {
+                alert("Error something went Wrong")
+            }
+        });
 
-// }
+    console.log(apiUrl);
+    console.log("here it is");
 
-// var displayFive = function (data, name) {
+}
+
+var displayFive = function (data) {
 
 
-// }
+    console.log(data.list[1]);
+    var day1date = document.querySelector(".day1date");
+    var day1temp = document.querySelector('.day1temp');
+    var day1humidity = document.querySelector('.day1humidity');
+    var day1wind = document.querySelector('.day1wind');
+
+    var date1 = moment().add(1, 'days').format('L');
+    var temp1 = data.list[1].main.temp;
+    var humid1 = data.list[1].main.humidity;
+    var wind1 = data.list[1].wind.speed;
+
+    day1date.innerHTML = date1;
+    day1temp.innerHTML = "Temperature: " + temp1 + "°F";
+    day1humidity.innerHTML = "Humidity: " + humid1 + "%";
+    day1wind.innerHTML = "Wind: " + wind1 + "mph";
+
+}
 
 
